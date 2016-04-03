f25be7306090c97992f712496a16a1bcfad86be3
evelin <eponce19@gmail.com>
2016-04-02 19:44:38 -0600

    Add rescue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,36 +1,42 @@
 class WelcomeController < ApplicationController
   def index
 
-    configuration = Moodle::Api::Configuration.new
-    configuration.host = 'htt://yourwebsite.com'
-    configuration.username = 'username'
-    configuration.password = 'password'
-    configuration.service = 'yourservice'
-    configuration
+    host = 'http://baticueva.com/moodle'
 
-    p Moodle::Api::TokenGenerator.new(configuration).call
-    
+    configuration          = Moodle::Api::Configuration.new
+    configuration.host     = host
+    configuration.username = 'batman'
+    configuration.password = 'ellasellamabamartha'
+    configuration.service  = 'justiceleague'
+    p configuration
+
+    token =  Moodle::Api::TokenGenerator.new(configuration).call
+
+    # The client can also be instantiated and used.
+    Moodle::Api.configure({host: host, token: token })
+
     #List all users
     params = { 'criteria[0][key]' => 'email', 'criteria[0][value]' => '%%' }
     @users = Moodle::Api.core_user_get_users(params)
     p @users
 
-    #Find only 1 user
-    params = { 'userids[0]' => '3'}
-    @user = Moodle::Api.core_user_get_users_by_id(params)
+    #Find authenthicated user
+    params = { 'criteria[0][key]' => 'username', 'criteria[0][value]' => configuration.username }
+    @user = Moodle::Api.core_user_get_users(params)
     p @user
 
     #List courses of a user
-    params = { 'userid' => '3' }
+    params = { 'userid' => '5' }
     @courses = Moodle::Api.core_enrol_get_users_courses(params)
     p @courses
 
     #List the users and roles in a course
-    params = { 'courseid' => '2' }
+    params = { 'courseid' => '5' }
     @course_users = Moodle::Api.core_enrol_get_enrolled_users(params)
     p @course_users
 
-     
+  rescue
+    @message = "Usuario no encontrado"
 
   end
 end
